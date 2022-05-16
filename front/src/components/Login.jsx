c850aa0b24045fb8af31e9ea7bee4bda0b7319de
Daniil Bukharev <daniil_buharev@mail.ru>
2022-05-16 18:49:27 +0300

    Lab 10

--- front/src/components/Login.jsx
+++ front/src/components/Login.jsx
@@ -2,13 +2,14 @@ import React, {useState} from 'react';
 import BackendService from "../services/BackendService";
 import Utils from "../utils/Utils";
 import {useNavigate} from "react-router-dom";
+import {connect} from "react-redux"
 
-export default function Login() {
+export default connect()(function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loggingIn, setLoggingIn] = useState(false);
     const [submitted, setSubmitted] = useState(false);
-    const [error_message, setErrorMessage] = useState(null);
+    //const [error_message, setErrorMessage] = useState(null);
     const nav = useNavigate();
 
     function handleChangeLogin(e) {
@@ -22,7 +23,7 @@ export default function Login() {
     function handleSubmit(e) {
         e.preventDefault();
         setSubmitted(true);
-        setErrorMessage(null);
+        //setErrorMessage(null);
         setLoggingIn(true);
         BackendService.login(username, password)
             .then ( resp => {
@@ -32,18 +33,18 @@ export default function Login() {
                 nav("/home");
             })
             .catch( err => {
-                if (err.response && err.response.status === 401)
-                    setErrorMessage("Ошибка авторизации");
-                else
-                    setErrorMessage(err.message);
+                //if (err.response && err.response.status === 401)
+                //    setErrorMessage("Ошибка авторизации");
+                //else
+                //    setErrorMessage(err.message);
                 setLoggingIn(false);
             })
     }
 
     return (
         <div className="col-md-6 me-0">
-            {error_message &&
-                <div className="alert alert-danger mt-1 me-0 ms-0">{error_message}</div>}
+            {/*error_message &&
+                <div className="alert alert-danger mt-1 me-0 ms-0">{error_message}</div>*/}
             <h2>Вход</h2>
             <form name="form" onSubmit={handleSubmit}>
                 <div className="form-group">
@@ -77,81 +78,4 @@ export default function Login() {
             </form>
         </div>
     );
-}
-
-/*class Login extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            loggingIn: false,
-            submitted: false,
-            error_message: null
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    render() {
-        console.log("render");
-        let { submitted, username, password, loggingIn } = this.state;
-        return (
-            <div className={"col-md-6 me-0"}>
-                {this.state.error_message &&
-                    <div className={"alert alert-danger mt-1 me-0 ms-0"}>{this.state.error_message}</div>}
-                <h2>Вход</h2>
-                <form name={"form"} onSubmit={this.handleSubmit}>
-                    <div className={"form-group"}>
-                        <label htmlFor={"username"}>Логин</label>
-                        <input type={"text"} className={'form-control'
-                            + (submitted && !username ? ' is-invalid' : '')}
-                               name={"username"} value={username} onChange={this.handleChange} />
-                        {submitted && !username && <div className={"help-block text-danger"}>Введите имя пользователя
-                        </div>}
-                    </div>
-                    <div className={"form-group"}>
-                        <label htmlFor={password}>Пароль</label>
-                        <input type={"password"} className={'form-control'
-                            + (submitted && !password ? ' is-invalid' : '')}
-                               name={"password"} value={password} onChange={this.handleChange} />
-                        {submitted && !password && <div className={"help-block text-danger"}>Введите пароль
-                        </div>}
-                    </div>
-                    <div className={"form-group mt-2"}>
-                        <button className={"btn btn-primary"}>
-                            {loggingIn && <span className={"spinner-border spinner-border-sm"} role={"status"}
-                                                aria-hidden={"true"}/>}
-                            Вход
-                        </button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-
-    handleChange(e) {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        const { username, password } = this.state;
-        BackendService.login(username, password)
-            .then( resp => {
-                console.log(resp.data);
-                Utils.saveUser(resp.data);
-            })
-            .catch()
-            .finally(() => this.setState({loggingIn : false}))
-            .catch( err => {
-                if (err.response && err.response.status === 401)
-                    this.setState( { error_message: "Ошибка авторизации", loggingIn: false });
-                else
-                    this.setState({ error_message: err.message, loggingIn: false });
-            });
-    }
-}*/
+})
--- front/src/components/NavigationBarClass.jsx
+++ front/src/components/NavigationBarClass.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {Nav, Navbar} from 'react-bootstrap'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 import {Link, useNavigate} from 'react-router-dom';
 import Utils from "../utils/Utils";
 import BackendService from "../services/BackendService";
+import {connect, useDispatch} from "react-redux";
+import {userActions} from "../utils/Rdx";
 
 class NavigationBarClass extends React.Component {
     constructor(props) {
@@ -20,45 +22,52 @@ class NavigationBarClass extends React.Component {
     logout() {
         BackendService.logout().then(() => {
             Utils.removeUser();
-            this.goHome();
+            this.props.dispatch(userActions.logout());
+            this.props.navigate('Login');
         })
     }
 
     render() {
-        let uname = Utils.getUserName();
         return (
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
+                <Navbar.Brand>myRPO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-auto">
+                        {/*<Nav.Link href="/home">Home</Nav.Link>*/}
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link onClick={this.goHome}>Another Home</Nav.Link>
-                        <Nav.Link onClick={() =>
-                            {this.props.navigate('/home');}
-                        }>Yet Another Home</Nav.Link>
+                        <Nav.Link onClick={this.goHome}>Another home</Nav.Link>
+                        <Nav.Link onClick={() => { this.props.history.push("/home")}} >Yet another home</Nav.Link>
                     </Nav>
-                    <Navbar.Text>{uname}</Navbar.Text>
-                    { uname &&
-                        <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth/>
-                            {' '}Выход
-                        </Nav.Link>
-                    }
-                    { !uname &&
-                        <Nav.Link as={Link} to={"/login"}><FontAwesomeIcon icon={faUser} fixedWidth/>
-                            {' '}Вход
-                        </Nav.Link>
-                    }
                 </Navbar.Collapse>
+                <Navbar.Text>{this.props.user && this.props.user.login}</Navbar.Text>
+                { this.props.user &&
+                    <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>
+                }
+                { !this.props.user &&
+                    <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>
+                }
             </Navbar>
         );
     }
 }
 
-
 const NavigationBar = props => {
     const navigate = useNavigate();
+    const uname = Utils.getUserName();
+    const dispatch = useDispatch();
 
-    return <NavigationBarClass navigate={navigate} {...props} />
+    return <NavigationBarClass
+        navigate={navigate}
+        uname={uname}
+        dispatch={dispatch}
+        {...props} />
 }
-export default NavigationBar;
+
+const mapStateToProps = state => {
+    const { user } = state.authentication;
+    return { user };
+}
+
+export default connect(mapStateToProps)(NavigationBar);
